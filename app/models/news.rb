545a6ece72af2b1d435fda93221f388bcd3a9c7a
Andrey N <dluhhbiu@gmail.com>
2019-08-18 15:30:29 +0300

    refactor: add validation to news model

--- app/models/news.rb
+++ app/models/news.rb
@@ -3,6 +3,15 @@
 class News < Sequel::Model
   include WithCreatedAt
 
+  plugin :validation_helpers
+
+  ALLOWED_FORMATS = %w[img text none].freeze
+
+  def validate
+    super
+    validates_includes(ALLOWED_FORMATS, :format)
+  end
+
   def img?
     self.format == 'img'
   end
